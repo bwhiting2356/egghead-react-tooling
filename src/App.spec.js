8eb5d9eebbd448ce370b0c16bd453e6024ca349b
Brendan Whiting <brendan.whiting@cognizant.com>
2019-01-27 19:41:59 -0500

    should pass

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import { render } from 'react-testing-library';
-import App from './App';
+import React from 'react'
+import { render } from 'react-testing-library'
+import App from './App'
 
 describe('App', () => {
-    it('Renders without error', () => {
-        render(<App/>);
-    })
-});
+  it('Renders without error', () => {
+    render(<App />)
+  })
+
+  // it('should fail', () => {
+  //   expect(true).toBe(false)
+  // })
+})
